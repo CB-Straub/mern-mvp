65401c55a16a9e667c975887655277c8f15de2d8
Brian Straub <cbstraub@protonmail.com>
2023-01-27 12:42:52 -0700

    proxy port changed to 8000

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const productRoutes = require('./routes/productRoutes')
 connectDB()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 app.use(express.json()) 
